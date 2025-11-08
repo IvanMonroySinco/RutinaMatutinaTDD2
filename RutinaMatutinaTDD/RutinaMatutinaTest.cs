073f234477dab6ebce8e6a5405fcf914b82a6ae1
Ivan Rene Monroy Santos <ivan.monroy@sinco.com.co>
2025-11-07 22:17:34 -0500

    🟥Dada_HoraActual6_00_Cuando_ConsultoQueDeboEstarHaciendoAhora_Debe_Retornar_HacerEjercicio

---
+++ RutinaMatutinaTDD/RutinaMatutinaTest.cs
@@ -0,0 +1,27 @@
+﻿using AwesomeAssertions;
+
+namespace RutinaMatutinaTDD;
+
+public class RutinaMatutinaTest
+{
+    [Fact]
+    public void Dada_HoraActual6_00_Cuando_ConsultoQueDeboEstarHaciendoAhora_Debe_Retornar_HacerEjercicio()
+    {
+        var rutinaMatutina = new RutinaMatutina();
+        rutinaMatutina.HoraActual = new TimeSpan(6, 0, 0);
+
+        var actividad = rutinaMatutina.QueDeboEstarHaciendoAhora();
+
+        actividad.Should().Be("Hacer ejercicio");
+    }
+}
+
+public class RutinaMatutina
+{
+    public TimeSpan HoraActual { get; set; }
+
+    public object QueDeboEstarHaciendoAhora()
+    {
+        throw new NotImplementedException();
+    }
+}
